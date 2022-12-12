38f30efc7cea786d908c6f499b5af8097639810e
nehasaipangallu <52908091+nehasaipangallu@users.noreply.github.com>
2022-12-12 22:48:24 +0530

    3 files modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import {
+  Route,
+  Routes,
+  BrowserRouter,
+  Switch,
+  HashRouter,
+} from 'react-router-dom';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <HashRouter>
+        
+        <Header />
+        <Routes>
+        <Route exact path="/home" element={<Home />}></Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
